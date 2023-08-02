207b48e7fa9bfa89d60acfb4f859b8899ee28018
eduardofloriani <eeduardofloriani@gmail.com>
2023-08-02 14:15:50 -0300

    invoice stack creation

--- bin/e_commerce_aws.ts
+++ bin/e_commerce_aws.ts
@@ -7,6 +7,7 @@ import { ProductsAppLayersStack } from '../lib/productsAppLayers-stack';
 import { EventsDdbStack } from '../lib/eventsDdb-stack';
 import { OrdersAppLayerStack } from '../lib/ordersAppLayer-stack';
 import { OrdersAppStack } from '../lib/ordersApp-stack';
+import { InvoiceWSApiStack } from '../lib/invoiceWSApi-stack';
 
 const app = new cdk.App();
 
@@ -63,3 +64,11 @@ const eCommerceApiStack = new ECommerceApiStack(app, 'ECommerceApi', {
 });
 eCommerceApiStack.addDependency(productsAppStack);
 eCommerceApiStack.addDependency(ordersAppStack);
+
+const invoiceWSApiStack = new InvoiceWSApiStack(app, 'InvoiceApi', {
+    tags: {
+        cost: 'InvoiceApp',
+        team: 'Eduardo',
+    },
+    env: env,
+});
